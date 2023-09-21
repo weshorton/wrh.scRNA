88886c8378d52bd3cb94fa2e027ced7d5f3803d5
Wes Horton <hortowe@ohsu.edu>
2023-09-21 13:44:24 -0700

    Add data .rda files and description man pages for treatment codes and other info about combo 4x project

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-###
-### Combo 4x scRNAseq Object Descriptions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-###
+#############################################
+### Combo 4x scRNAseq Object Descriptions ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#############################################
 
 #' Major Population Colors
 #' 
@@ -13,3 +13,249 @@
 #'    \item{names}{Population Name}
 #' }
 "mPopColors_v"
+
+###
+### BATCH 1+2 POPULATIONS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' Batch1+2 Lymphoid Colors
+#' 
+#' Colors used on UMAPs for b1+b2 lymphoid populations
+#' 
+#' @format ## `b12_lymphoidColors_v`
+#' Named vector of length 11
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"b12_lymphoidColors_v"
+
+#' Batch1+2 Lymphoid Colors 2
+#' 
+#' Colors used on UMAPs for b1+b2 lymphoid populations 
+#' when collapsing like sub-pops.
+#' 
+#' @format ## `b12_lymphoid2Colors_v`
+#' Named vector of length 6
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Collapsed Sub Population Name}
+#' }
+"b12_lymphoid2Colors_v"
+
+#' Batch1+2 Myeloid Colors
+#' 
+#' Colors used on UMAPs for b1+b2 myeloid populations
+#' 
+#' @format ## `b12_myeloidColors_v`
+#' Named vector of length 7
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"b12_myeloidColors_v"
+
+#' Batch1+2 Myeloid Colors 2
+#' 
+#' Colors used on UMAPs for b1+b2 myeloid populations 
+#' when collapsing like sub-pops.
+#' 
+#' @format ## `b12_myeloid2Colors_v`
+#' Named vector of length 6
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Collapsed Sub Population Name}
+#' }
+"b12_myeloid2Colors_v"
+
+###
+### BATCH 3 POPULATIONS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' Batch3 Lymphoid Colors
+#' 
+#' Colors used on UMAPs for b3 lymphoid populations
+#' 
+#' @format ## `b3_lymphoidColors_v`
+#' Named vector of length 12
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"b3_lymphoidColors_v"
+
+#' Batch3 Lymphoid Colors 2
+#' 
+#' Colors used on UMAPs for b3 lymphoid populations 
+#' when collapsing like sub-pops.
+#' 
+#' @format ## `b3_lymphoid2Colors_v`
+#' Named vector of length 5
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Collapsed Sub Population Name}
+#' }
+"b3_lymphoid2Colors_v"
+
+#' Batch3 Myeloid Colors
+#' 
+#' Colors used on UMAPs for b3 myeloid populations
+#' 
+#' @format ## `b3_myeloidColors_v`
+#' Named vector of length 11
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"b3_myeloidColors_v"
+
+#' Batch3 Myeloid Colors 2
+#' 
+#' Colors used on UMAPs for b3 myeloid populations 
+#' when collapsing like sub-pops.
+#' 
+#' @format ## `b3_myeloid2Colors_v`
+#' Named vector of length 7
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Collapsed Sub Population Name}
+#' }
+"b3_myeloid2Colors_v"
+
+###
+### TREATMENT COLORS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' Treatment Colors
+#' 
+#' Colors used on plots for treatments
+#' 
+#' @format ## `treatColors_v`
+#' Named vector of length 9
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Treatment Name}
+#' }
+"treatColors_v"
+
+###
+### BATCH 1+2 OBJECT ATTRIBUTES  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' Batch1+2 Cluster names
+#' 
+#' Column names found in batch12 seurat object
+#' metadata that contain the clusters identified
+#' from the Seurat analysis pipeline. The names
+#' are formated "seurat_clusters_d[dim]_res[res]
+#' where dim = dimensions used in reduction and
+#' res = resolution used for clustering
+#' 
+#' @format ## `b12_clusterNames_v`
+#' Named vector of length 4
+#' \describe{
+#'    \item{values}{meta.data column name}
+#'    \item{names}{seurat object name}
+#' }
+"b12_clusterNames_v"
+
+#' Batch1+2 UMAP names
+#' 
+#' UMAP names found in names() of batch12 seurat object
+#' 
+#' @format ## `b12_umapNames_v`
+#' Named vector of length 4
+#' \describe{
+#'    \item{values}{umap name from seurat object}
+#'    \item{names}{seurat object name}
+#' }
+"b12_clusterNames_v"
+
+#' Batch1+2 Resolution values
+#' 
+#' Resolution values used for clustering in batch12 seurat object
+#' 
+#' @format ## `b12_res_v`
+#' Named vector of length 4
+#' \describe{
+#'    \item{values}{resolution}
+#'    \item{names}{seurat object name}
+#' }
+"b12_clusterNames_v"
+
+###
+### BATCH 3 OBJECT ATTRIBUTES  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' Batch3 Cluster names
+#' 
+#' Column names found in batch3 seurat object
+#' metadata that contain the clusters identified
+#' from the Seurat analysis pipeline. The names
+#' are formated "seurat_clusters_d[dim]_res[res]
+#' where dim = dimensions used in reduction and
+#' res = resolution used for clustering
+#' 
+#' @format ## `b3_clusterNames_v`
+#' Named vector of length 4
+#' \describe{
+#'    \item{values}{meta.data column name}
+#'    \item{names}{seurat object name}
+#' }
+"b3_clusterNames_v"
+
+#' Batch3 UMAP names
+#' 
+#' UMAP names found in names() of batch3 seurat object
+#' 
+#' @format ## `b3_umapNames_v`
+#' Named vector of length 4
+#' \describe{
+#'    \item{values}{umap name from seurat object}
+#'    \item{names}{seurat object name}
+#' }
+"b3_clusterNames_v"
+
+#' Batch3 Resolution values
+#' 
+#' Resolution values used for clustering in batch3 seurat object
+#' 
+#' @format ## `b3_res_v`
+#' Named vector of length 4
+#' \describe{
+#'    \item{values}{resolution}
+#'    \item{names}{seurat object name}
+#' }
+"b3_clusterNames_v"
+
+###
+### MISC STUFF  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' WES Sample - Treatment Mapping
+#' 
+#' data.table that maps sample IDs from Whole Exome
+#' Sequencing to treatment labels
+#' 
+#' @format ## `wesSample_scRNATreat_dt`
+#' data.table with 22 rows and 2 columns
+#' \describe{
+#'    \item{Tumor_Sample_Barcode}{Sample ID in WES data}
+#'    \item{Treatment}{Treatment code}
+#' }
+"wesSample_scRNATreat_dt"
+
+#' scRNA/scATAC Treatment Code Mapping
+#' 
+#' data.table that maps shorthand treatment
+#' codes to long-form "_"-separated codes. Note
+#' that PTX_ENT_aCSF1R_unt is in sc
+#' 
+#' @format ## `fullAbbrTreatmentMap_dt`
+#' data.table with 22 rows and 2 columns
+#' \describe{
+#'    \item{Treatment}{scRNAseq Treatment Code}
+#'    \item{longTreatment}{long-form treatment code used in scATAC}
+#'    \item{Name}{Full Rx Name}
+#' }
+"fullAbbrTreatmentMap_dt"
