96b1c4d867159b2340ca94ccebccf9dee910eaae
Wes Horton <hortowe@ohsu.edu>
2023-09-21 12:48:27 -0700

    Try adding package data and description

---
+++ R/data.R
@@ -0,0 +1,14 @@
+###
+### Combo 4x scRNAseq Object Descriptions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' Major Population Colors
+#' 
+#' Colors used on UMAPs for major populations
+#' 
+#' @format ## `mPopColors_v`
+#' Named vector of length 6
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \itme{names}{Population Name}
+"mPopColors_v"
