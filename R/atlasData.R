6a7db43a0664d987c04c9956192197462876aa79
weshorton <wrhorton592@gmail.com>
2024-02-05 16:57:17 -0800

    Add all of the atlas colors as package data

---
+++ R/atlasData.R
@@ -0,0 +1,154 @@
+#############################################
+### Atlas scRNAseq Object Descriptions ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#############################################
+
+###
+### BATCH 2 POPULATIONS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' Batch2 Lymphoid Colors
+#' 
+#' Colors used on UMAPs for b2 lymphoid populations
+#' 
+#' @format ## `atlas_b2_lymphoidColors_v`
+#' Named vector of length 5
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"atlas_b2_lymphoidColors_v"
+
+#' Batch2 Lymphoid Colors 2
+#' 
+#' Colors used on UMAPs for b2 lymphoid populations 
+#' second option.
+#' 
+#' @format ## `atlas_b2_lymphoidColors2_v`
+#' Named vector of length 5
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"atlas_b2_lymphoidColors2_v"
+
+#' Batch2 Myeloid Colors
+#' 
+#' Colors used on UMAPs for b2 myeloid populations
+#' 
+#' @format ## `atlas_b2_myeloidColors_v`
+#' Named vector of length 6
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"atlas_b2_myeloidColors_v"
+
+#' Batch2 Myeloid Colors 2
+#' 
+#' Colors used on UMAPs for b2 myeloid populations 
+#' when collapsing like sub-pops.
+#' 
+#' @format ## `atlas_b2_myeloidColors2_v`
+#' Named vector of length 7
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"atlas_b2_myeloidColors2_v"
+
+#' Batch2 Neoplastic Colors
+#' 
+#' Colors used on UMAPs for b2 neplastic populations 
+#' 
+#' @format ## `atlas_b2_neoplasticColors_v`
+#' Named vector of length 5
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name (neo.c#)}
+#' }
+"atlas_b2_neoplasticColors_v"
+
+#' Batch2 Other Colors
+#' 
+#' Colors used on UMAPs for b2 other populations 
+#' 
+#' @format ## `atlas_b2_otherColors_v`
+#' Named vector of length 3
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"atlas_b2_otherColors_v"
+
+###
+### BATCH 3 POPULATIONS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+#' Batch3 Lymphoid Colors
+#' 
+#' Colors used on UMAPs for b3 lymphoid populations
+#' 
+#' @format ## `atlas_b3_lymphoidColors_v`
+#' Named vector of length 5
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"atlas_b3_lymphoidColors_v"
+
+
+#' Batch3 Myeloid Colors
+#' 
+#' Colors used on UMAPs for b3 myeloid populations
+#' 
+#' @format ## `atlas_b3_myeloidColors_v`
+#' Named vector of length 6
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"atlas_b3_myeloidColors_v"
+
+#' Batch3 Neoplastic Colors
+#' 
+#' Colors used on UMAPs for b3 neplastic populations 
+#' 
+#' @format ## `atlas_b3_neoplasticColors_v`
+#' Named vector of length 5
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name (neo.c#)}
+#' }
+"atlas_b3_neoplasticColors_v"
+
+#' Batch3 Other Colors
+#' 
+#' Colors used on UMAPs for b3 other populations 
+#' 
+#' @format ## `atlas_b3_otherColors_v`
+#' Named vector of length 3
+#' \describe{
+#'    \item{values}{Hex Color}
+#'    \item{names}{Sub Population Name}
+#' }
+"atlas_b3_otherColors_v"
+
+###
+### GENE LISTS  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+###
+
+
+#' Atlas Gene Lists
+#' 
+#' Gene lists used for atlas project
+#' 1. 15 MMTvPyMT genes
+#' 1. 6 'neoplastic' genes
+#' 1. 1 'myeloid' gene
+#' 
+#' @format ## `atlasGeneLists_lsv`
+#' Named list of length 3
+#' \describe{
+#'    \item{values}{vector of gene names}
+#'    \itme{names}{name of gene list}
+#' }
+"atlasGeneLists_lsv"
