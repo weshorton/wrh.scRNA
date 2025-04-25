fc0a02af8842a6d1d7fc1c27bb0351c353f7d5b3
weshorton <wrhorton592@gmail.com>
2025-04-25 13:45:53 -0700

    Update umapFigureTheme and add new transparent theme

--- R/ggThemes.R
+++ R/ggThemes.R
@@ -7,9 +7,9 @@ umapFigureTheme <- function() {
     theme(plot.title = element_text(hjust = 0.5, size = 36),
           axis.text = element_text(size = 24),
           axis.title = element_text(size = 28),
-          axis.ticks = element_line(linewidth = 2),
+          axis.ticks = element_line(linewidth = 0.25),
           axis.ticks.length = unit(0.25, "cm"),
-          axis.line = element_line(linewidth = 2),
+          axis.line = element_line(linewidth = 0.25),
           strip.text = element_text(size = 24),
           legend.text = element_text(size = 24),
           legend.title = element_text(size = 28))
@@ -62,3 +62,18 @@ figureDotTheme <- function() {
     
 } # figureDotTheme
 
+transparentTheme <- function() {
+  #' Transparent Everything
+  #' @description try to make as few artifacts as possible
+  
+  my_theme() + theme(
+    panel.background = element_rect(fill = "transparent", 
+                                    colour = NA_character_), # necessary to avoid drawing panel outline
+    panel.grid.major = element_blank(), # get rid of major grid
+    panel.grid.minor = element_blank(), # get rid of minor grid
+    plot.background = element_rect(fill = "transparent",
+                                   colour = NA_character_), # necessary to avoid drawing plot outline
+    legend.background = element_rect(fill = "transparent"),
+    legend.box.background = element_rect(fill = "transparent"),
+    legend.key = element_rect(fill = "transparent"))
+}
